ee2d1f4034ad6abb7cc20d31030517d59034dc01
Dmitry Cherkasov <govor86@gmail.com>
2017-03-14 20:40:10 +0200

    renamed ViewController. Added TableViewController.

---
+++ Checklists/ChecklistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Dmitry Cherkasov on 3/14/17.
+//  Copyright © 2017 Dmitry Cherkasov. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
