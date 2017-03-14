9f692637bbbfbc072bc9cbbd343a0cd30a784ccc
Dmitry Cherkasov <govor86@gmail.com>
2017-03-14 21:38:37 +0200

    added: check/uncheck functional generated 100 items

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -20,6 +20,44 @@ class ChecklistViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+//------------------------------------------------------------------------------
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
+        let label = tableView.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if  indexPath.row % 5 == 1{
+            label.text = "Commit some new features"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Read 50 pages"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Search for the job"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Send CV to vacancies from favorites"
+        }
+        
+        return cell
+    }
+    
+    
 }
 
